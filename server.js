c5940b88a986e0ca5212b880921382b52c30c702
mbatorek7 <maegan.batorek@valpo.edu>
2023-03-21 22:13:21 -0500

    added functions for each query and created init function

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const consoleTable = require('console.table'); 
 const inquirer = require('inquirer');
-// Import and require mysql2
 const mysql = require('mysql2');
 
-require('dotenv').config()
+//import functions to be used for each inquirer choice
+import { viewDepartments, viewRoles, viewEmployees, addDepartment, addEmployee, addRole, updateEmployeeRole } from "./queryFunctions";;
 
-const PORT = process.env.PORT || 3001;
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
@@ -17,3 +17,55 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
 );
+
+function init() {
+  inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'choices', 
+      message: 'What would you like to do?',
+      choices: ['View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role',
+                'No Action']
+    }
+  ])
+  .then((answers) => {
+    switch(answers) {
+      case 'View all departments':
+        viewDepartments();
+        break;
+
+      case 'View all roles':
+        viewRoles();
+        break;
+
+      case 'View all employees':
+        viewEmployees();
+        break;
+
+      case 'Add a department':
+        addDepartment();
+        break;
+
+      case 'Add a role':
+        addRole();
+        break;
+
+      case 'Add am employee':
+        addEmployee();
+        break;
+        
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+        
+      case 'No Action':
+        
+    }
+  })
+}
